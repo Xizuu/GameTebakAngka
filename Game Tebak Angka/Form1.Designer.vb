7054bdce166ecb3a601d5e9484dcb038ebe636e0
Raihan Pratama Putra <98379156+Xizuu@users.noreply.github.com>
2024-06-13 09:18:49 +0800

    Add files via upload

---
+++ Game Tebak Angka/Form1.Designer.vb
@@ -0,0 +1,134 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        txtAngkaSatu = New TextBox()
+        txtAngkaDua = New TextBox()
+        txtOperator = New TextBox()
+        btnMulai = New Button()
+        btnJawab = New Button()
+        txtJawaban = New TextBox()
+        Label2 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(51, 25)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(202, 30)
+        Label1.TabIndex = 0
+        Label1.Text = "Game Tebak Angka"
+        ' 
+        ' txtAngkaSatu
+        ' 
+        txtAngkaSatu.Location = New Point(98, 95)
+        txtAngkaSatu.Name = "txtAngkaSatu"
+        txtAngkaSatu.Size = New Size(100, 23)
+        txtAngkaSatu.TabIndex = 1
+        txtAngkaSatu.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' txtAngkaDua
+        ' 
+        txtAngkaDua.Location = New Point(98, 153)
+        txtAngkaDua.Name = "txtAngkaDua"
+        txtAngkaDua.Size = New Size(100, 23)
+        txtAngkaDua.TabIndex = 2
+        txtAngkaDua.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' txtOperator
+        ' 
+        txtOperator.Location = New Point(132, 124)
+        txtOperator.Name = "txtOperator"
+        txtOperator.Size = New Size(28, 23)
+        txtOperator.TabIndex = 3
+        txtOperator.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' btnMulai
+        ' 
+        btnMulai.Location = New Point(51, 253)
+        btnMulai.Name = "btnMulai"
+        btnMulai.Size = New Size(75, 23)
+        btnMulai.TabIndex = 4
+        btnMulai.Text = "Mulai"
+        btnMulai.UseVisualStyleBackColor = True
+        ' 
+        ' btnJawab
+        ' 
+        btnJawab.Location = New Point(178, 253)
+        btnJawab.Name = "btnJawab"
+        btnJawab.Size = New Size(75, 23)
+        btnJawab.TabIndex = 5
+        btnJawab.Text = "Jawab"
+        btnJawab.UseVisualStyleBackColor = True
+        ' 
+        ' txtJawaban
+        ' 
+        txtJawaban.Location = New Point(98, 224)
+        txtJawaban.Name = "txtJawaban"
+        txtJawaban.Size = New Size(100, 23)
+        txtJawaban.TabIndex = 6
+        txtJawaban.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label2.Location = New Point(139, 188)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(21, 21)
+        Label2.TabIndex = 7
+        Label2.Text = "="
+        Label2.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(320, 450)
+        Controls.Add(Label2)
+        Controls.Add(txtJawaban)
+        Controls.Add(btnJawab)
+        Controls.Add(btnMulai)
+        Controls.Add(txtOperator)
+        Controls.Add(txtAngkaDua)
+        Controls.Add(txtAngkaSatu)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtAngkaSatu As TextBox
+    Friend WithEvents txtAngkaDua As TextBox
+    Friend WithEvents txtOperator As TextBox
+    Friend WithEvents btnMulai As Button
+    Friend WithEvents btnJawab As Button
+    Friend WithEvents txtJawaban As TextBox
+    Friend WithEvents Label2 As Label
+
+End Class
---
+++ Game Tebak Angka/Form1.vb
@@ -0,0 +1,95 @@
+﻿Public Class Form1
+
+    Dim gameStarted As Boolean = False
+
+    Private Sub btnMulai_Click(sender As Object, e As EventArgs) Handles btnMulai.Click
+        If gameStarted Then
+            Return
+        End If
+        gameStarted = True
+
+        Dim angkaAcak1 As List(Of Integer) = AcakAngka()
+        txtAngkaSatu.Text = angkaAcak1(0).ToString()
+
+        txtOperator.Text = AcakOperator()
+
+        Dim angkaAcak2 As List(Of Integer) = AcakAngka()
+        txtAngkaDua.Text = angkaAcak2(0).ToString()
+    End Sub
+
+    Private Sub btnJawab_Click(sender As Object, e As EventArgs) Handles btnJawab.Click
+        Dim angkaSatu As Integer
+        If Not Integer.TryParse(txtAngkaSatu.Text, angkaSatu) Then
+            MessageBox.Show("Masukkan angka yang valid untuk Angka Satu")
+            Return
+        End If
+
+        Dim angkaDua As Integer
+        If Not Integer.TryParse(txtAngkaDua.Text, angkaDua) Then
+            MessageBox.Show("Masukkan angka yang valid untuk Angka Dua")
+            Return
+        End If
+
+        Dim operatorStr As String = txtOperator.Text.Trim()
+        Dim hasil As Integer
+
+        Select Case operatorStr
+            Case "+"
+                hasil = angkaSatu + angkaDua
+            Case "-"
+                hasil = angkaSatu - angkaDua
+            Case "*"
+                hasil = angkaSatu * angkaDua
+            Case Else
+                MessageBox.Show("Operator tidak valid")
+                Return
+        End Select
+
+        Dim jawaban As Integer
+        If Not Integer.TryParse(txtJawaban.Text, jawaban) Then
+            MessageBox.Show("Masukkan jawaban yang valid")
+            Return
+        End If
+
+        If hasil = jawaban Then
+            MessageBox.Show("Jawaban benar!")
+            clearAll()
+        Else
+            MessageBox.Show("Jawaban salah. Coba lagi!")
+        End If
+    End Sub
+
+    Function AcakAngka() As List(Of Integer)
+        Dim angkaList As New List(Of Integer)
+        For i As Integer = 1 To 20
+            angkaList.Add(i)
+        Next
+
+        Dim rng As New Random()
+        Dim n As Integer = angkaList.Count
+        While n > 1
+            n -= 1
+            Dim k As Integer = rng.Next(n + 1)
+            Dim value As Integer = angkaList(k)
+            angkaList(k) = angkaList(n)
+            angkaList(n) = value
+        End While
+
+        Return angkaList
+    End Function
+
+    Function AcakOperator() As String
+        Dim operatorList As New List(Of String) From {"+", "-", "*"}
+        Dim rng As New Random()
+        Dim index As Integer = rng.Next(operatorList.Count)
+        Return operatorList(index)
+    End Function
+
+    Private Sub clearAll()
+        txtAngkaSatu.Text = Nothing
+        txtAngkaDua.Text = Nothing
+        txtOperator.Text = Nothing
+        txtJawaban.Text = Nothing
+        gameStarted = False
+    End Sub
+End Class
